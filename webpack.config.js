c3e6eb4c39be82521e8e65cdf28aed142d9ca6f6
Mou <dhmourelle@gmail.com>
2017-10-10 19:17:04 +0200

    Initial commit. Add Users and Events endpoints

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: ['whatwg-fetch','./lib/fpapi.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'fpapi.min.js',
+    libraryTarget: 'umd',
+    library: 'FPAPI'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+  //  new webpack.optimize.UglifyJsPlugin(),
+    new webpack.ProvidePlugin({  Promise: 'es6-promise'  })
+  ]
+}
